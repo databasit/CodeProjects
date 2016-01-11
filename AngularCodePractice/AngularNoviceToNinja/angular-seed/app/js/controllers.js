d6e0263736b237e4130a994d183d9f2c39f0eec2
dartabasit <indetrd@gmail.com>
2016-01-11 14:26:07 -0400

    full change

--- AngularCodePractice/AngularNoviceToNinja/angular-seed/app/js/controllers.js
+++ AngularCodePractice/AngularNoviceToNinja/angular-seed/app/js/controllers.js
@@ -1,11 +1,11 @@
 'use strict';
 
 /* Controllers */
-
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+controller('FinanceController', function($scope) {
+  $scope.salary = 0;
+  $scope.percentage = 0;
+  $scope.result = function() {
+    return $scope.salary * $scope.percentage * 0.01;
+  };
+});
